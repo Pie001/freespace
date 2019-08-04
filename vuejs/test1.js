c857aeec9a715046ddc2cddbaa99c77931e37aab
pie <papamau09@gmail.com>
2019-08-04 21:57:18 +0900

    Add vue.js test1

---
+++ vuejs/test1.js
@@ -0,0 +1,26 @@
+var vm = new Vue({
+  el: '#example',
+  data: {
+    message: '안녕하세요'
+  },
+  computed: {
+    // 계산된 getter
+    reversedMessage: function () {
+      // `this` 는 vm 인스턴스를 가리킵니다.
+      return this.message.split('').reverse().join('')
+    }
+  }
+})
+
+var vm2 = new Vue({
+  el: '#demo',
+  data: {
+    firstName: 'Foo',
+    lastName: 'Bar'
+  },
+  computed: {
+    fullName: function () {
+      return this.firstName + ' ' + this.lastName
+    }
+  }
+})
